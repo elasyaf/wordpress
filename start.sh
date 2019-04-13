1f3260e5a18bb7c0ddbf38b7fbf0e6bc5a0a08ae
elasyaf <xtriv3r@gmail.com>
2019-04-13 16:27:01 +0700

    using sed for overwrite wp-config.php

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+sed -i 's/database_name_here/new-text/g' /var/www/wordpress/wp-config.php
+sed -i 's/username_here/new-text/g' /var/www/wordpress/wp-config.php
+sed -i 's/password_here/new-text/g' /var/www/wordpress/wp-config.php
+sed -i 's/localhost/new-text/g' /var/www/wordpress/wp-config.php
+
 service nginx start
 service php7.1-fpm start
 while true; do sleep 1000; done
