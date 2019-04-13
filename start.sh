f5df562fba4ba5f0878f38a0a2d35807452541f9
elasyaf <xtriv3r@gmail.com>
2019-04-13 16:33:05 +0700

    rollback start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-sed -i 's/database_name_here/$WP_DATABASE/g' /var/www/wordpress/wp-config.php
-sed -i 's/username_here/$WP_USER/g' /var/www/wordpress/wp-config.php
-sed -i 's/password_here/$WP_PASSWORD/g' /var/www/wordpress/wp-config.php
-sed -i 's/localhost/$WP_DB_HOST/g' /var/www/wordpress/wp-config.php
-
 service nginx start
 service php7.1-fpm start
 while true; do sleep 1000; done
