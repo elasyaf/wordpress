c77419d8bb24e91da5c37a79f07d6dafb45349bd
elasyaf <xtriv3r@gmail.com>
2019-04-13 16:28:27 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-sed -i 's/database_name_here/new-text/g' /var/www/wordpress/wp-config.php
-sed -i 's/username_here/new-text/g' /var/www/wordpress/wp-config.php
-sed -i 's/password_here/new-text/g' /var/www/wordpress/wp-config.php
-sed -i 's/localhost/new-text/g' /var/www/wordpress/wp-config.php
+sed -i 's/database_name_here/$WP_DATABASE/g' /var/www/wordpress/wp-config.php
+sed -i 's/username_here/$WP_USER/g' /var/www/wordpress/wp-config.php
+sed -i 's/password_here/$WP_PASSWORD/g' /var/www/wordpress/wp-config.php
+sed -i 's/localhost/$WP_DB_HOST/g' /var/www/wordpress/wp-config.php
 
 service nginx start
 service php7.1-fpm start
